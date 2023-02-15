c0f06d543f1cb841beac917561bde66cb34250fe
hyeonjin <104902715+hyeonjin6530@users.noreply.github.com>
2023-02-16 00:30:19 +0900

    [Silver IV] Title: 큐, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+import sys
+
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop(0))  # pop(0)은 리스트의 첫 번째 요소를 리턴, 제거
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
