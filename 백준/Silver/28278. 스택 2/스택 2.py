205627be7ae469584f0e714641b2fdb5cd6d9c20
hyeonjin <104902715+hyeonjin6530@users.noreply.github.com>
2023-08-13 22:38:03 +0900

    [Silver IV] Title: 스택 2, Time: 864 ms, Memory: 102992 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == "1":
+        stack.append(command[1])
+    elif command[0] == "2":
+        if len(stack) != 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "3":
+        print(len(stack))
+    elif command[0] == "4":
+        if len(stack) != 0:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "5":
+        if len(stack) != 0:
+            print(stack[-1])
+        else:
+            print(-1)
