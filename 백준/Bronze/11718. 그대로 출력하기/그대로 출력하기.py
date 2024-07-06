4a231f247ba084ba0f1c325ee780fa6941389b4b
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2024-07-07 01:52:00 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -3,3 +3,7 @@ while True:
         print(input())
     except:
         break
+
+# sys.stdin.readline()을 사용하면 출력초과가 발생한다.
+# input()는 EOF를 받을 때 EOFError가 발생한다.
+# 그러나 sys.stdin.readline()은 EOF를 받을 때 빈 문자열을 반환한다.
