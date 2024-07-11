569be4344fef13e7ac5271d221c05cf4289cd1c4
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2024-07-12 02:49:33 +0900

    [Bronze III] Title: 대지, Time: 1360 ms, Memory: 35060 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,10 @@
+num = int(input())
+xList = []
+yList = []
+
+for i in range(num):
+    x, y = map(int, input().split())
+    xList.append(x)
+    yList.append(y)
+
+print((max(xList) - min(xList)) * (max(yList) - min(yList)))
