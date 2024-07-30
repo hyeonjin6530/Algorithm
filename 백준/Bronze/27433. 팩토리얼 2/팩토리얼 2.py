11dd0cf6c445d917e83f73ad85122a122f9a63cb
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2024-07-30 22:37:33 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,12 @@
+# 재귀로 풀어보기
+import sys
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(n))
