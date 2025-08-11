ad87a32308ab1010563c6643ae9498782430c409
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2025-08-11 17:36:43 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,11 @@
-# 시간초과로 인해 list 보다 deque를 사용하는 것이 빠르다.
 from collections import deque
-import sys
 
-n = int(sys.stdin.readline())
+n = int(input())
 
-card = deque(i for i in range(1, n+1))
+queue = deque(i for i in range(n, 0, -1))
 
-while len(card) > 1:
-    card.popleft()  # 첫 번째 원소를 제거
-    card.rotate(-1)  # 원소를 맨 뒷자리로 이동시킴
+while len(queue) > 1:
+  queue.pop()
+  queue.appendleft(queue.pop())
 
-print(card[0])
+print(queue.pop())
