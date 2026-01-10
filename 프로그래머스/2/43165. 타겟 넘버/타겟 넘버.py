f229887fa77f9af3e1cc9f32170c278983fec34e
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-01-11 00:36:43 +0900

    [level 2] Title: 타겟 넘버, Time: 350.59 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,17 @@
+answer = 0
+
+def dfs(idx, current, numbers, target):
+    global answer
+
+    if idx == len(numbers):
+        if current == target:
+            answer += 1
+        return answer
+
+    dfs(idx+1, current + numbers[idx],numbers, target)
+    dfs(idx+1, current - numbers[idx], numbers, target)
+
 def solution(numbers, target):
-    answer = 0
-    
-    def dfs(idx, current):
-        nonlocal answer
-
-        if idx == len(numbers):
-            if current == target:
-                answer += 1
-            return answer
-
-        dfs(idx+1, current + numbers[idx])
-        dfs(idx+1, current - numbers[idx])
-    
-    dfs(0, 0)
+    dfs(0, 0, numbers, target)
     
     return answer
