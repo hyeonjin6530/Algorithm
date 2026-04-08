84f91abed27f18d8a11b956adb82343c412562df
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-04-09 00:24:54 +0900

    [Silver III] Title: N과 M (5), Time: 136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+visited = [False] * n
+
+# 조합
+def backtracking(arr):
+  if len(arr) == m:
+    print(' '.join(map(str, arr)))
+    return
+  
+  for i in range(n):
+    if visited[i] == False:
+      visited[i] = True
+      backtracking(arr + [nums[i]])
+      visited[i] = False
+
+backtracking([])
