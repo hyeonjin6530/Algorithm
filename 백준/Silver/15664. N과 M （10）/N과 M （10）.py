de531e6c4344d06b6cda786ab42d8dccf7b3b2f6
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-04-09 01:08:59 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+# 조합
+def backtracking(idx, arr):
+  if len(arr) == m:
+    print(' '.join(map(str, arr)))
+    return
+  
+  # 같은 depth에서 같은 값을 선택할 수 없도록
+  prev = None
+  for i in range(idx, n):
+    if nums[i] != prev:
+      prev = nums[i]
+      backtracking(i+1, arr + [nums[i]])
+
+backtracking(0, [])
