e24bf31ec00f837d15a6647dd52ab31de58d1427
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-04-09 00:20:10 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,18 +1,12 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-arr = []
 
+# 중복조합
+def backtracking(idx, arr):
+  if len(arr) == m:
+    print(' '.join(map(str, arr)))
+    return
+  
+  for i in range(idx, n + 1):
+    backtracking(i, arr + [i])
 
-def backtracking():
-    if len(arr) == m:
-        print(' '.join(map(str, arr)))
-        return
-    for i in range(1, n+1): 
-        if len(arr) == 0 or arr[-1] <= i:
-            arr.append(i)
-            backtracking()
-            arr.pop()
-
-backtracking()
+backtracking(1, [])
