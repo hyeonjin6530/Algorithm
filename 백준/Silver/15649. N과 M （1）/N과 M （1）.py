48ce60d4141b4ec0e468ff99a5e4ba554dbfaf6e
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-04-08 23:09:41 +0900

    [Silver III] Title: N과 M (1), Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,17 @@
-import sys
-input = sys.stdin.readline
-
-
 n, m = map(int, input().split())
-arr = []
 
-def backtracking():
-    if len(arr) == m:
-        print(' '.join(map(str, arr)))
-        return
-    for i in range(1, n+1):
-        if i not in arr:
-            arr.append(i)
-            backtracking()
-            arr.pop()
+visited = [False] * (n+1)
 
-backtracking()
+def backtracking(arr):
+  if len(arr) == m:
+    print(' '.join(map(str, arr)))
+    return
+  
+  for i in range(1, n+1):
+    if visited[i] == False:
+      visited[i] = True  # 방문!
+      backtracking(arr + [i])
+      # 제거해줘야지 나중에 또 방문할 수 있기 때문에 [1, 2] 방문하고 2를 제거해줘야지 [1, 3] 이렇게 방문이 가능하다.
+      visited[i] = False
+
+backtracking([])
