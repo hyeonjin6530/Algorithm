1661e2389d91090a4aa223f1516eb43ad8db6693
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-04-26 21:24:08 +0900

    [level 3] Title: 가장 먼 노드, Time: 42.20 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    
+    # 간선 정보 리스트 한 사이즈 더 크게 만들기
+    graph = [[] for _ in range(n+1)]
+    
+    # 간선 정보 저장하기
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    # 거리를 저장할 그래프
+    dist = [1e9] * (n+1)
+    dist[0] = 0
+    
+    visited = [False] * (n+1)
+        
+    q = deque()
+    q.append(1)
+    dist[1] = 0
+    visited[1] = True
+    
+    while q:
+        now = q.popleft()
+        
+        for i in graph[now]:
+            if visited[i] == False:
+                dist[i] = min(dist[i] , dist[now] + 1)
+                visited[i] = True
+                q.append(i)
+    
+    max_dist = max(dist)
+
+    for i in dist:
+        if i == max_dist:
+            answer += 1
+    
+    return answer
