6448385ca9fe9ab1a8269b9767557ee3c53fbd06
hyeonjin6530 <cola1876@naver.com>
2026-05-14 14:21:29 +0900

    docs: 디렉토리 구조 변경

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,24 +3,38 @@ class Solution:
         stack = []
 
         # 해결법 1 : 모든 경우를 나눠서 체크 해보기 (겹치는 부분이 많아서 뭔가 비효율적으로 보임)
+        # for i in s:
+        #     if i == '(' or i == '{' or i == '[':
+        #         stack.append(i)
+        #     elif i == ')':
+        #         if len(stack) > 0 and stack[-1] == '(':
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     elif i == '}':
+        #         if len(stack) > 0 and stack[-1] == '{':
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     elif i == ']':
+        #         if len(stack) > 0 and stack[-1] == '[':
+        #             stack.pop()
+        #         else:
+        #             return False
+
+        # 해결법 2 : 딕셔너리를 이용한 방법
+        dict = { ')' : '(', '}' : '{', ']' : '['}
+
         for i in s:
-            if i == '(' or i == '{' or i == '[':
+            # 여는 괄호
+            if i in dict.values():
                 stack.append(i)
-            elif i == ')':
-                if len(stack) > 0 and stack[-1] == '(':
-                    stack.pop()
-                else:
+            
+            # 닫는 괄호
+            else:
+                if not stack or stack[-1] != dict[i]:
                     return False
-            elif i == '}':
-                if len(stack) > 0 and stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
-            elif i == ']':
-                if len(stack) > 0 and stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
+                stack.pop()
         
         if len(stack) > 0:
             return False
