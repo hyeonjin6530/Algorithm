97fc1a353757a9dfb9307961796ce70eab1a373b
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-05-20 03:39:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.66 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 3.66 ms
+메모리: 9.16 MB, 시간: 1.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 29일 00:47:41
+2026년 05월 20일 03:39:18
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,49 +1,34 @@
-# 전선을 하나씩 끊으면서 bfs를 돌려서 한 쪽의 개수만 세면 된다!
-from collections import deque
-
 def solution(n, wires):
-    answer = 1e9
+    answer = n
     
-    # bfs 함수
-    def bfs(start, a):
+    # 인접 리스트로 변환
+    graph = [[] for _ in range(n+1)]
+    
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    # wires를 순회하며 하나씩 지워보기
+    for a, b in wires:
         visited = [False] * (n+1)
         
-        q = deque()
-        q.append(start)
-        visited[start] = True
-        
-        count = 1
-        
-        while q:
-            now = q.popleft()
+        def dfs(node):
+            visited[node] = True
+            count = 1
             
-            for i in a[now]:
+            for i in graph[node]:
+                if (node == a and i == b) or (node == b and i == a):
+                    continue
+                
                 if not visited[i]:
-                    visited[i] = True
-                    q.append(i)
-                    count += 1
-                    
-        return count
-    
-    answer_list = set()
-    
-    # 돌아가면서 링크를 끊어준다
-    for i in range(n-1):
-        arr = wires[:i] + wires[i+1:]
+                    count += dfs(i)
+            
+            return count
         
-         # 전선 정보를 저장할 인접리스트
-        link = [[] for _ in range(n+1)]
-    
-        # 돌아가면서 정보 저장
-        for v1, v2 in arr:
-            link[v1].append(v2)
-            link[v2].append(v1)
+        cnt = dfs(1)
         
-        # 어차피 2개로 나뉘니까 for문 돌릴 필요없이 1만 돌리면 된다.
-        answer_list.add(bfs(1, link))
-    
-    # 가장 차이가 적게 나는 정답 고르기
-    for i in answer_list:
-        answer = min(answer, abs(i - (n-i)))
+        diff = abs(cnt - (n-cnt))
+        
+        answer = min(answer, diff)      
     
     return answer
