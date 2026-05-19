3131a25694a0fc538fd412f70c99003440189bab
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-05-20 01:36:42 +0900

    [level 2] Title: 타겟 넘버, Time: 180.78 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 350.59 ms
+메모리: 9.21 MB, 시간: 180.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 11일 00:36:41
+2026년 05월 20일 01:36:37
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,21 @@
-answer = 0
-
-def dfs(idx, current, numbers, target):
-    global answer
-
-    if idx == len(numbers):
-        if current == target:
-            answer += 1
-        return answer
-
-    dfs(idx+1, current + numbers[idx],numbers, target)
-    dfs(idx+1, current - numbers[idx], numbers, target)
-
 def solution(numbers, target):
-    dfs(0, 0, numbers, target)
+    answer = 0
+    
+    def dfs(idx, result):
+        nonlocal answer
+        
+        # 끝까지 다 계산했을 때
+        if idx == len(numbers):
+            if result == target:
+                answer += 1
+            return
+        
+        # 다음 숫자를 더할 때
+        dfs(idx + 1, result + numbers[idx])
+        
+        # 다음 숫자를 뺄 때
+        dfs(idx + 1, result - numbers[idx])
+        
+    dfs(0, 0)
     
     return answer
