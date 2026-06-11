d62ff50615057e13ff9f054e8c93646917e45566
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-06-11 15:27:17 +0900

    [level 2] Title: 기능개발, Time: 0.27 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 0.06 ms
+메모리: 11.5 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 06일 11:37:05
+2026년 06월 11일 15:27:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,26 @@
+from collections import deque
+
 def solution(progresses, speeds):
     answer = []
     
-    # 작업이 끝나는 날
-    day = []
+    works = deque(progresses)
+    doWorks = deque(speeds)
     
-    # 작업일수를 계산해서 저장
-    for i in range(len(progresses)):
-        result = (100 - progresses[i]) // speeds[i]
+    # 모든 작업이 끝날 때까지 반복
+    while works:
         
-        if (100 - progresses[i]) % speeds[i] == 0:
-            day.append(result)
-        else:
-            day.append(result + 1)
-    
-    # 다음 요소가 지금 요소보다 클 때까지 계속 pop(0)
-    while len(day) > 0:
-        now = day.pop(0)
-        count = 1
+        count = 0
         
-        while len(day) > 0 and now >= day[0]:
-            day.pop(0)
-            count += 1
-            
-        answer.append(count)
-    
+        # 만약 맨 앞의 작업이 끝났을 경우에만 끝난 작업을 다 꺼냄
+        if works[0] >= 100:
+            while works and works[0] >= 100:
+                works.popleft()
+                doWorks.popleft()
+                count += 1
+            answer.append(count)
+        
+        # 작업 진도 + 작업 속도 해주기
+        for i in range(len(works)):
+            works[i] += doWorks[i]
+        
     return answer
